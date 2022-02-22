1c8bf8fc7abd7967af76aec8a78eefb45fb80283
Caleb-Stripes <calebdstripes@gmail.com>
2022-02-21 21:20:24 -0500

    initial thoughts

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //var iFrame = get element by id 0 and update it's src with a random url from the url array file
+
+// function random_item(items)
+// {
+  
+// return items[Math.floor(Math.random()*items.length)];
+     
+// }
+
+// var items = [254, 45, 212, 365, 2543];
+// console.log(random_item(items));
+
+const myContent = Document.getElementById("zero").src;
+myContent = "https://www.w3schools.com/jsref/prop_img_src.asp";
