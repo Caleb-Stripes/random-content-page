90ab77bf51b8d1ce232b2b4ee17d087b84ae6bd6
Caleb-Stripes <calebdstripes@gmail.com>
2022-03-08 18:47:39 -0500

    second solution

--- index.js
+++ index.js
@@ -1,45 +1,61 @@
-// function random_item(items)
-// {
-  
-// return items[Math.floor(Math.random()*items.length)];
-     
-// }
-
-// var items = [254, 45, 212, 365, 2543];
-// console.log(random_item(items));
-//var iFrameSource = document.getElementById('zero').src; <-this does not set the src when in the function, rather it only stores it as a value
+// Array of src urls from pool of content
 const srcArray = [];
 srcArray[0] = 'https://docs.google.com/presentation/d/1C9R1uJ3UxTYtFIvq5gJJqNN4mo3kpLpk-dIRYZLBVXc/preview?rm=minimal&slide=id.g1163cbbd3f2_0_0',
 srcArray[1] = 'https://docs.google.com/presentation/d/1C9R1uJ3UxTYtFIvq5gJJqNN4mo3kpLpk-dIRYZLBVXc/preview?rm=minimal&slide=id.g1163cbbd3f2_0_254',
 srcArray[2] = 'https://docs.google.com/presentation/d/1C9R1uJ3UxTYtFIvq5gJJqNN4mo3kpLpk-dIRYZLBVXc/preview?rm=minimal&slide=id.g1163cbbd3f2_0_5'
 
+// Array to store used src objects for refilling srcArray when it is empty.
+const srcArray2 = [];
+
 //function to grab random array url
 function myFunction(items) {
-    //console.log("hello"); 1
-    //console.log(iFrameSource); 2
     return items[Math.floor(Math.random()*items.length)];
 }
 
-//establish the first src
+//establish the first src on page load
 function srcSet(){
     document.getElementById('zero').src = myFunction(srcArray);
 }
 
-//console.log(myFunction(srcArray)); 3
-function srcSwap(){
-    //variable to store the new src
-    var newId = myFunction(srcArray);
-    //variable to store the current id
-    var currentId = document.getElementById('zero').src;
-    //check to make sure the new id is not the same as the current before updating the src
-    if(newId == currentId) {
-        return srcSwap()
-    } else {
-        document.getElementById('zero').src = newId;
-    }
-
+//function to return a random id from the srcArray for use in removing the object and setting the src of the iframe
+function randomId(items) {
+    return Math.floor(Math.random()*items.length);
 }
 
+// //swap the src to a different random option
+// function srcSwap(){
+//     //variable to store the new src
+//     var newId = myFunction(srcArray);
+//     //variable to store the current id
+//     var currentId = document.getElementById('zero').src;
+//     //check to make sure the new id is not the same as the current before updating the src
+//     if(newId == currentId) {
+//         return srcSwap()
+//     } else {
+//         document.getElementById('zero').src = newId;
+//     }
+
+// }
+
 srcSet();
+
+//another solution for randomization that doesn't repeat until all have been used then if array is empty refills it
+function srcSwap(){
+    var newId = randomId(srcArray);
+    
+    //gets the url from srcArray and sets the iFrame src with it
+    var newUrl = srcArray[newId];
+    document.getElementById('zero').src = newUrl;
+    
+    //removes the object from srcArray so it is not repeated
+    srcArray.splice(newId, newId);
+
+    //adds the object to the restoration array
+    srcArray2.push(newUrl);
+
+    //if the srcArray is empty refills it with the objects of srcArray2 and then empties srcArray2
+    
+}
+
 //runs the shuffle constantly, works just comment out for development
-setInterval(srcSwap, 3000);
+//setInterval(srcSwap, 8000);
