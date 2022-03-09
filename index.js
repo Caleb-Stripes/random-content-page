9a27d25b15a3edf7fca15389ba981b65f7e9f0cf
Caleb-Stripes <calebdstripes@gmail.com>
2022-03-09 07:34:23 -0500

    solution works error free just need to clean it up

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const srcArray = [];
 srcArray[0] = 'https://docs.google.com/presentation/d/1C9R1uJ3UxTYtFIvq5gJJqNN4mo3kpLpk-dIRYZLBVXc/preview?rm=minimal&slide=id.g1163cbbd3f2_0_0',
 srcArray[1] = 'https://docs.google.com/presentation/d/1C9R1uJ3UxTYtFIvq5gJJqNN4mo3kpLpk-dIRYZLBVXc/preview?rm=minimal&slide=id.g1163cbbd3f2_0_254',
-srcArray[2] = 'https://docs.google.com/presentation/d/1C9R1uJ3UxTYtFIvq5gJJqNN4mo3kpLpk-dIRYZLBVXc/preview?rm=minimal&slide=id.g1163cbbd3f2_0_5'
+srcArray[2] = 'https://docs.google.com/presentation/d/1C9R1uJ3UxTYtFIvq5gJJqNN4mo3kpLpk-dIRYZLBVXc/preview?rm=minimal&slide=id.g1163cbbd3f2_0_5',
+srcArray[3] = 'https://www.w3schools.com/js/js_if_else.asp',
+srcArray[4] = 'https://www.w3schools.com/jsref/met_loc_reload.asp'
 
 //an array to work with as the functions run
 let array2 = srcArray;
@@ -26,15 +28,17 @@ function shuffle(array) {
   }
 
   function srcSwap(){
-      //shuffle sources
-      shuffle(array2);
-      //take the last one for the iFrame src
-      document.getElementById('zero').src = array2.pop();
       //check if the array is empty and refresh it
-      if (array2.length < 1) {
-          array2 = srcArray;
+      if (array2.length <= 1) {
+          location.reload();
       }
 
+      //shuffle sources
+      shuffle(array2);
+
+      //take the last one for the iFrame src
+      document.getElementById('zero').src = array2.pop();
   }
 
   srcSwap();
+  setInterval(srcSwap, 3000);
