8067192bb831682cb124b95f7e764121a86c25b1
Caleb-Stripes <calebdstripes@gmail.com>
2022-03-08 23:08:04 -0500

    second solution final

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ srcArray[1] = 'https://docs.google.com/presentation/d/1C9R1uJ3UxTYtFIvq5gJJqNN4m
 srcArray[2] = 'https://docs.google.com/presentation/d/1C9R1uJ3UxTYtFIvq5gJJqNN4mo3kpLpk-dIRYZLBVXc/preview?rm=minimal&slide=id.g1163cbbd3f2_0_5'
 
 // Array to store used src objects for refilling srcArray when it is empty.
-const srcArray2 = [];
+const srcArray2 = srcArray;
 
 //function to grab random array url
 function myFunction(items) {
@@ -17,7 +17,7 @@ function srcSet(){
     document.getElementById('zero').src = myFunction(srcArray);
 }
 
-//function to return a random id from the srcArray for use in removing the object and setting the src of the iframe
+//function to return a random id from the srcArray2 for use in removing the object and setting the src of the iframe
 function randomId(items) {
     return Math.floor(Math.random()*items.length);
 }
@@ -41,20 +41,20 @@ srcSet();
 
 //another solution for randomization that doesn't repeat until all have been used then if array is empty refills it
 function srcSwap(){
-    var newId = randomId(srcArray);
+    var newId = randomId(srcArray2);
     
     //gets the url from srcArray and sets the iFrame src with it
-    var newUrl = srcArray[newId];
+    var newUrl = srcArray2[newId];
     document.getElementById('zero').src = newUrl;
     
     //removes the object from srcArray so it is not repeated
-    srcArray.splice(newId, newId);
-
-    //adds the object to the restoration array
-    srcArray2.push(newUrl);
+    srcArray2.splice(newId, newId);
 
     //if the srcArray is empty refills it with the objects of srcArray2 and then empties srcArray2
-    
+    if (srcArray2.length == 1) {
+        srcArray2.pop();
+        srcArray2 = srcArray;
+    }
 }
 
 //runs the shuffle constantly, works just comment out for development
